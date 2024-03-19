89c285c095bb61dfa586d167142c842c316ddca5
fzhange <28616695353@qq.com>
2024-03-19 13:10:34 +0800

    feat: support nx release

--- modules/orders/src/order-list/order-list.vue
+++ modules/orders/src/order-list/order-list.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+import { Button } from '@nx-vue-cosmopolis/shared-ui';
+
 // defineProps<{}>()
 </script>
 
 <template>
+  <Button></Button>
+
   <p>Welcome to OrderList!</p>
 </template>
 
--- modules/products/src/product-list/product-list.vue
+++ modules/products/src/product-list/product-list.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import { Button } from '@nx-vue-cosmopolis/shared-ui';
 // defineProps<{}>()
 </script>
 
 <template>
+  <Button></Button>
   <p>Welcome to ProductList!</p>
 </template>
 
